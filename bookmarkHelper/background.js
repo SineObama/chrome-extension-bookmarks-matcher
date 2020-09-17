b07657e0f540ef0356ce2766eb38b2e944999eaf
Sine <714186139@qq.com>
2020-09-17 20:07:22 +0800

    【更新】增加应用图标悬浮显示收藏情况；优化图标。

--- bookmarkHelper/background.js
+++ bookmarkHelper/background.js
@@ -2,6 +2,18 @@ tabs = {};
 tabIds = [];
 bookmarkMap = {};
 
+function brief(s, length) {
+    length = length || 18;
+    if (length < 5) {
+        length = 5;
+    }
+    if (s.length > length) {
+        return s.substr(0, length - 3) + '...';
+    } else {
+        return s;
+    }
+}
+
 function loadWindowList() {
     chrome.windows.getAll({populate: true}, function (windowList) {
         tabs = {};
@@ -17,10 +29,27 @@ function loadWindowList() {
         for (var i = 0; i < tabIds.length; i++) {
             (function (i) {
                 chrome.tabs.get(tabIds[i], function (tab) {
-                    if (bookmarkMap[normalizeUrl(tab.url)]) {
+                    var items = bookmarkMap[normalizeUrl(tab.url)];
+                    if (items) {
                         chrome.pageAction.show(tabIds[i]);
+                        var markInfo = items.length > 1 ? '【已收藏多个！】' : '【已收藏】';
+                        if (items.length > 2) {
+                            items = items.slice(0, 2);
+                        }
+                        var title = '智能书签识别\n' + markInfo + '\n';
+                        for (var j = 0; j < items.length; j++) {
+                            var item = items[j];
+                            var itemInfo = '';
+                            for (var k = 0; k < item.parents.length; k++) {
+                                itemInfo += '/' + item.parents[k];
+                            }
+                            itemInfo += '/\n' + brief(item.title) + '\n';
+                            title += itemInfo;
+                        }
+                        chrome.pageAction.setTitle({tabId: tabIds[i], title: title});
                     } else {
                         chrome.pageAction.hide(tabIds[i]);
+                        chrome.pageAction.setTitle({tabId: tabIds[i], title: '智能书签识别\n【当前网页未收藏】'});
                     }
                 });
             })(i);
@@ -52,13 +81,28 @@ function normalizeUrl(url) {
     return url;
 }
 
-function loadBookmarksTree(nodes) {
+function loadBookmarksTree(nodes, parentTitles) {
+    parentTitles = parentTitles || [];
     for (var i = 0; i < nodes.length; i++) {
         var node = nodes[i];
         if (node.url) {
-            bookmarkMap[normalizeUrl(node.url)] = 1;
+            var item = {
+                parents: parentTitles,
+                title: node.title,
+                url: node.url
+            };
+            var key = normalizeUrl(node.url);
+            var items = bookmarkMap[key];
+            if (!items) {
+                bookmarkMap[key] = [item];
+            } else {
+                items.push(item);
+            }
         } else if (node.children) {
-            loadBookmarksTree(node.children);
+            var newTitles = [];
+            newTitles.push.apply(newTitles, parentTitles);
+            newTitles.push(node.title);
+            loadBookmarksTree(node.children, newTitles);
         }
     }
 }
